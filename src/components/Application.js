cbcbf564232087c4751d4947c2852478ad00359a
Simen Brekken <simen@unfold.no>
2017-02-22 19:55:04 +0100

    WIP: Styling

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,19 +1,29 @@
 import React from 'react'
-import { StatusBar } from 'react-native'
+import { Platform, StatusBar } from 'react-native'
 import styled from 'styled-components/native'
 
 import NowPlaying from './NowPlaying'
 import Setup from './Setup'
 import Zones from './Zones'
 
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : 0
+
 const Container = styled.View`
   flex-grow: 1;
   background-color: black;
 `
 
+const StatusBarContainer = styled.View`
+  height: ${STATUSBAR_HEIGHT};
+  background-color: black;
+`
+
 export default () => (
   <Container>
-    <StatusBar barStyle="dark-content" />
+    <StatusBarContainer>
+      <StatusBar backgroundColor="blue" barStyle="light-content" />
+    </StatusBarContainer>
+
     <NowPlaying />
     <Zones />
     <Setup />
