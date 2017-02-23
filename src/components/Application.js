4235160d29be48e98e32ccca56aef541f5d0aad6
Morten Harnang <mharnang@gmail.com>
2017-02-23 10:26:48 +0100

    Removed status bar for now

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,29 +1,17 @@
 import React from 'react'
-import { Platform, StatusBar } from 'react-native'
 import styled from 'styled-components/native'
 
 import NowPlaying from './NowPlaying'
 import Setup from './Setup'
 import Zones from './Zones'
 
-const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : 0
-
 const Container = styled.View`
   flex-grow: 1;
   background-color: black;
 `
 
-const StatusBarContainer = styled.View`
-  height: ${STATUSBAR_HEIGHT};
-  background-color: black;
-`
-
 export default () => (
   <Container>
-    <StatusBarContainer>
-      <StatusBar backgroundColor="blue" barStyle="light-content" />
-    </StatusBarContainer>
-
     <NowPlaying />
     <Zones />
     <Setup />
